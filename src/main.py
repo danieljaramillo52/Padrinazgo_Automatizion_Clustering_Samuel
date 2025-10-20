c0f3f4d03664a662b30f0de0ba9ee8e1721f83a2
Samuel Molina <samumoe2401@gmail.com>
2025-10-19 21:21:58 -0500

    Lectura de todo los archivos .xlsm

---
+++ src/main.py
@@ -0,0 +1,21 @@
+import pandas as pd 
+
+df_u_directa = pd.read_excel(r"C:\Users\samuel.molina\Padrinazgo_Automatizion_Clustering_Samuel\Insumos\Universo Directa.xlsm",
+                             sheet_name = "Maestra")
+#print(df_u_directa)
+#print("----------------------------")   #Revicioón de tabla completa
+print(df_u_directa.columns)
+
+df_u_directa = df_u_directa.iloc[:, list(range(7)) + [22]]
+
+#print(df_u_directa)
+#print("----------------------------")
+#print(df_u_directa.columns)
+
+df_u_indirecta = pd.read_excel(r"C:\Users\samuel.molina\Padrinazgo_Automatizion_Clustering_Samuel\Insumos\Universo Indirecta.xlsm")
+
+#print(df_u_indirecta)
+
+df_Bas_Socio = pd.read_excel(r"C:\Users\samuel.molina\Padrinazgo_Automatizion_Clustering_Samuel\Insumos\BaseSocios.xlsm")
+
+print(df_Bas_Socio.columns)
