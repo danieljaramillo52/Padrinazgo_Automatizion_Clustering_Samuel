c77d19a299ee53b0b73c3efb4d90d8ef3c2f6d4e
Samuel Molina <samumoe2401@gmail.com>
2025-11-24 18:33:46 -0500

    mostrar archivos

--- iniciar.bat
+++ iniciar.bat
@@ -2,16 +2,16 @@
 echo ===============================
 echo Configurando el proyecto Python...
 
-attrib +h +s ".vscode" /s /d
-attrib +h +s "Scripts" /s /d
-attrib +h +s "src" /s /d
-attrib +h +s "Controllers" /s /d
-attrib +h +s "python-3.12.6-emb.zip" /s /d
-attrib +h +s "python-3.12.6-emb" /s /d
-attrib +h +s "Utils" /s /d
-attrib +h +s ".gitignore"
-attrib +h +s "requirements.txt"
-attrib +h +s "iniciar.bat"
+attrib -h -s ".vscode" /s /d
+attrib -h -s "Scripts" /s /d
+attrib -h -s "src" /s /d
+attrib -h -s "Controllers" /s /d
+attrib -h -s "python-3.12.6-emb.zip" /s /d
+attrib -h -s "python-3.12.6-emb" /s /d
+attrib -h -s "Utils" /s /d
+attrib -h -s ".gitignore"
+attrib -h -s "requirements.txt"
+attrib -h -s "iniciar.bat"
 
 cd "python-3.12.6-emb"
 
